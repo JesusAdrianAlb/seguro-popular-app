f11b6100085c93e73669db3ff8ace666b00db4cc
Jesus Albornoz <ja.albornoz@hotmail.com>
2018-10-29 11:47:06 -0600

    Firt push, set up Ionic Pro and error handler

---
+++ src/app/app.module.ts
@@ -0,0 +1,57 @@
+import { NgModule, ErrorHandler, Injectable, Injector } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { Pro } from '@ionic/pro';
+import { IonicErrorHandler } from 'ionic-angular';
+
+Pro.init('decca4ae', {
+  appVersion: '0.0.1'
+});
+
+
+@Injectable()
+export class CustumErrorHandler implements ErrorHandler {
+  ionicErrorHandler: IonicErrorHandler;
+
+  constructor(injector: Injector) {
+    try {
+      this.ionicErrorHandler = injector.get(IonicErrorHandler);
+    } catch (e) {
+
+    }
+  }
+
+  handleError(error: any) {
+    Pro.monitoring.handleNewError(error);
+    // this.ionicErrorHandler && this.ionicErrorHandler.handleError(error);
+  }
+
+}
+
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    IonicErrorHandler,
+    [{provide: ErrorHandler, useClass: CustumErrorHandler}]
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}

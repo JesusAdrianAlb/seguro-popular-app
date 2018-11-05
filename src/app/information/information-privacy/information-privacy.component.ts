80e8148351337f87759c5b06f0037b6dc4aeebbf
Jesus Albornoz <ja.albornoz@hotmail.com>
2018-11-05 13:19:22 -0600

    InformationPrivacy: Creation of component

---
+++ src/app/information/information-privacy/information-privacy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-information-privacy',
+  templateUrl: './information-privacy.component.html',
+  styleUrls: ['./information-privacy.component.scss']
+})
+export class InformationPrivacyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

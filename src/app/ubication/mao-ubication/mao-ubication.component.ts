5c1b009985bc67cb71ecc29b083a6fb4826c4bf0
Jesus Albornoz <ja.albornoz@hotmail.com>
2018-11-05 13:21:38 -0600

    MaoUbication: Creation of component

---
+++ src/app/ubication/mao-ubication/mao-ubication.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mao-ubication',
+  templateUrl: './mao-ubication.component.html',
+  styleUrls: ['./mao-ubication.component.scss']
+})
+export class MaoUbicationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

a0069887da9148bc2ee4ce19598455319ee8d5e2
Jesus Albornoz <ja.albornoz@hotmail.com>
2018-11-05 13:20:52 -0600

    UbicationModule: Adding routes to module

--- src/app/ubication/ubication.module.ts
+++ src/app/ubication/ubication.module.ts
@@ -1,19 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { CluesUbicationComponent } from './clues-ubication/clues-ubication.component';
 import { BasicModule } from '../common/basic.module';
+import { MaoUbicationComponent } from './mao-ubication/mao-ubication.component';
+import { UbicationContainerComponent } from './ubication-container/ubication-container.component';
+import { UbicationMenuComponent } from './ubication-menu/ubication-menu.component';
+
+
+const routes: Routes = [
+    {
+      path: '',
+      component: UbicationContainerComponent,
+      children: [
+        { path: '', component: UbicationMenuComponent },
+        { path: 'mao-location', component: MaoUbicationComponent },
+        { path: 'clues-location', component: CluesUbicationComponent }
+      ]
+    },
+];
 
 @NgModule({
   imports: [
     CommonModule,
     BasicModule,
-    RouterModule.forChild([{
-      path: '',
-      component: CluesUbicationComponent
-    }])
+    RouterModule.forChild(routes)
   ],
-  declarations: [CluesUbicationComponent],
+  declarations: [
+    CluesUbicationComponent,
+    MaoUbicationComponent,
+    UbicationContainerComponent,
+    UbicationMenuComponent
+  ],
   providers: [
   ]
 })

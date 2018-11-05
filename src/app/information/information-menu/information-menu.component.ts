2cd55b999eda37771f48231044e09d57fdf67cbf
Jesus Albornoz <ja.albornoz@hotmail.com>
2018-11-05 13:19:07 -0600

    InformationMenu: Creation of component

---
+++ src/app/information/information-menu/information-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-information-menu',
+  templateUrl: './information-menu.component.html',
+  styleUrls: ['./information-menu.component.scss']
+})
+export class InformationMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

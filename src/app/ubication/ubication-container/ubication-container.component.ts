7041d3396b0da8093ce1fb3078972ad70a25bf58
Jesus Albornoz <ja.albornoz@hotmail.com>
2018-11-05 13:22:09 -0600

    UbicationMenu and Container: Creation of component

---
+++ src/app/ubication/ubication-container/ubication-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ubication-container',
+  templateUrl: './ubication-container.component.html',
+  styleUrls: ['./ubication-container.component.scss']
+})
+export class UbicationContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/ubication/ubication-menu/ubication-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ubication-menu',
+  templateUrl: './ubication-menu.component.html',
+  styleUrls: ['./ubication-menu.component.scss']
+})
+export class UbicationMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

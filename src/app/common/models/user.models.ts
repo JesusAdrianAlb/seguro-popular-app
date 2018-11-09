cc98600ced558c19cfb1500dd4203d2c10470625
Jesus Albornoz <ja.albornoz@hotmail.com>
2018-11-09 16:56:19 -0600

    Loggin service

---
+++ src/app/common/models/user.models.ts
@@ -0,0 +1,31 @@
+
+/**
+ * Representa la información simple de un usuario
+ */export interface User {
+    /**
+     * Nombre del usuario
+     */
+    name?: string;
+    /**
+     * Curp de usuario
+     */
+    curp?: string;
+    /**
+     * Folio de seguro popular
+     */
+    folio?: string;
+    /**
+     * Edad del usuario
+     */
+    age?: number;
+    /**
+     * Género del usuario
+     */
+    gender?: string;
+    /**
+     * Fecha de nacimiento
+     */
+    dateOfBirth?: Date;
+
+
+}
--- src/app/common/services/auth.service.ts
+++ src/app/common/services/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  user: User;
   constructor() { }
+
+  setLogginData(data: User) {
+    this.user = data;
+  }
+
+  getLogginData() {
+    return this.user;
+  }
 }
